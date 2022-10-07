60ea2c4f481dd2e9812e4a946d633d9cb9844238
Roman Sckorokhodov <i@rskorohodov.ru>
2022-10-07 19:30:23 +0300

    fix:правки №2

--- scripts/index.js
+++ scripts/index.js
@@ -2,26 +2,33 @@ const popupOpenButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close');
 const popupSaveButton = document.querySelector('.popup__button');
-function popupToggle() {
-    popup.classList.toggle('popup_opened');
-}
 
-let formElement =  document.querySelector('.popup__editor');
-let nameInput = document.querySelector('.popup__text_name');
-let jobInput = document.querySelector('.popup__text_job');
+let formElement = document.querySelector('.popup__editor');
+let nameInput = document.querySelector('.popup__input_text_name');
+let jobInput = document.querySelector('.popup__input_text_job');
 let nameTitle = document.querySelector('.profile__heading');
 let nameDescription = document.querySelector('.profile__description');
 
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
+nameTitle.textContent = 'Жак-Ив Кусто';
+nameDescription.textContent = 'Исследователь дна';
+
+function popupOpen() {
+    popup.classList.add('popup_opened');
+    nameInput.value = nameTitle.textContent;
+    jobInput.value = nameDescription.textContent;
+};
+
+function popupClose() {
+    popup.classList.remove('popup_opened');
+};
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
     nameTitle.textContent = nameInput.value;
     nameDescription.textContent = jobInput.value;
-    nameInput.placeholder = 'Ваше Имя';
-    jobInput.placeholder = 'Ваша должность';
-}
+};
 
-popupCloseButton.addEventListener('click', popupToggle); 
-popupOpenButton.addEventListener('click', popupToggle); 
-popupSaveButton.addEventListener('click', popupToggle); 
+popupCloseButton.addEventListener('click', popupClose);
+popupOpenButton.addEventListener('click', popupOpen);
 
 formElement.addEventListener('submit', formSubmitHandler); 
